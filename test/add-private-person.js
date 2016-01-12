6394d9a1ba4aace7e240a3ef774d293c7b56a502
maccyber <jonas.enge@gmail.com>
2016-01-12 17:13:08 +0100

    Added more tests

--- test/add-private-person.js
+++ test/add-private-person.js
@@ -77,3 +77,108 @@ tap.throws(
   {message: 'Missing required input: options.LastName'},
   'requires options.LastName to be supplied'
 )
+
+tap.throws(
+  function () {
+    var options = {
+      Enterprise: '985994196',
+      FirstName: 'Jonas',
+      MiddleName: 'Huseby',
+      LastName: 'Enge',
+      PersonalIdNumber: false
+    }
+    generator(options)
+  },
+  {message: 'Missing required input: options.PersonalIdNumber'},
+  'requires options.PersonalIdNumber to be supplied'
+)
+
+tap.throws(
+  function () {
+    var options = {
+      Enterprise: '985994196',
+      FirstName: 'Jonas',
+      MiddleName: 'Huseby',
+      LastName: 'Enge',
+      PersonalIdNumber: '26018645146',
+      Email: false
+    }
+    generator(options)
+  },
+  {message: 'Missing required input: options.Email'},
+  'requires options.Email to be supplied'
+)
+
+tap.throws(
+  function () {
+    var options = {
+      Enterprise: '985994196',
+      FirstName: 'Jonas',
+      MiddleName: 'Huseby',
+      LastName: 'Enge',
+      PersonalIdNumber: '26018645146',
+      Email: 'jonas.enge@gmail.com',
+      StreetAddress: false
+    }
+    generator(options)
+  },
+  {message: 'Missing required input: options.StreetAddress'},
+  'requires options.StreetAddress to be supplied'
+)
+
+tap.throws(
+  function () {
+    var options = {
+      Enterprise: '985994196',
+      FirstName: 'Jonas',
+      MiddleName: 'Huseby',
+      LastName: 'Enge',
+      PersonalIdNumber: '26018645146',
+      Email: 'jonas.enge@gmail.com',
+      StreetAddress: 'Grenlandsgata 35b',
+      ZipCode: false
+    }
+    generator(options)
+  },
+  {message: 'Missing required input: options.ZipCode'},
+  'requires options.ZipCode to be supplied'
+)
+
+tap.throws(
+  function () {
+    var options = {
+      Enterprise: '985994196',
+      FirstName: 'Jonas',
+      MiddleName: 'Huseby',
+      LastName: 'Enge',
+      PersonalIdNumber: '26018645146',
+      Email: 'jonas.enge@gmail.com',
+      StreetAddress: 'Grenlandsgata 35b',
+      ZipCode: '3912',
+      ZipPlace: false
+    }
+    generator(options)
+  },
+  {message: 'Missing required input: options.ZipPlace'},
+  'requires options.ZipPlace to be supplied'
+)
+
+tap.throws(
+  function () {
+    var options = {
+      Enterprise: '985994196',
+      FirstName: 'Jonas',
+      MiddleName: 'Huseby',
+      LastName: 'Enge',
+      PersonalIdNumber: '26018645146',
+      Email: 'jonas.enge@gmail.com',
+      StreetAddress: 'Grenlandsgata 35b',
+      ZipCode: '3912',
+      ZipPlace: 'Porsgrunn',
+      Area: false
+    }
+    generator(options)
+  },
+  {message: 'Missing required input: options.Area'},
+  'requires options.Area to be supplied'
+)
