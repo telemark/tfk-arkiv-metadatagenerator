a05ed21aa52430c6300942f8cf61803f4aae045a
maccyber <jonas.enge@gmail.com>
2016-01-12 20:37:01 +0100

    Added get-private-persons-name

---
+++ test/get-private-persons-name.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var tap = require('tap')
+var generator = require('../lib/get-private-persons-name')
+var testOptions = {
+  Name: 'Hermod'
+}
+var metaData = generator(testOptions)
+
+tap.equal(metaData.data.parameter.Name, testOptions.Name, 'Name is correct')
+
+tap.throws(
+  function () {
+    var options = false
+    generator(options)
+  },
+  {message: 'Missing required input: options'},
+  'requires an options object'
+)
+
+tap.throws(
+  function () {
+    var options = {
+      Name: false
+    }
+    generator(options)
+  },
+  {message: 'Missing required input: options.Name'},
+  'requires options.Name to be supplied'
+)
